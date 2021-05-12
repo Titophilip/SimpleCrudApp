d4762a733bcaf1d82893e0d1d63f6f2ac0ba40a7
Tito <ebenirot@gmail.com>
2021-05-12 23:51:03 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const studentRoutes = require('./routes/studentRoutes');
+
+// PORT
+require('dotenv').config();
+const port = process.env.PORT || PORT;
+
+// Setup database
+const db = require('./database/setup');
+db();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use(studentRoutes);
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome!' });
+});
+
+// listening to port
+app.listen(port, () => {
+    console.log(`app running on port ${port}`);
+});
